099c32212c5ddf063d918b683e4e82bab6160832
Pu <75680936+ginlorddd@users.noreply.github.com>
2026-04-08 09:40:54 +0700

    Fix grid data-column detection when Select column exists

--- FRM/FRM_PRODUCTION_PLAN.cs
+++ FRM/FRM_PRODUCTION_PLAN.cs
@@ -615,7 +615,7 @@ namespace DM_OHD.FRM
         private void EnsureColumnsAndStt(GridView view)
         {
             bool hasDataColumn = view.Columns.Cast<GridColumn>()
-                .Any(c => c.FieldName != "STT");
+                .Any(c => c.FieldName != "STT" && c.FieldName != SelectFieldName);
             if (!hasDataColumn)
             {
                 view.PopulateColumns();
