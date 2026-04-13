737ee9fb7773ce3783f8ca15bbf08d2c8c172b93
Pu <75680936+ginlorddd@users.noreply.github.com>
2026-04-13 14:20:08 +0700

    Hide BASE_OHD before 30M cumulative threshold

--- DTO/ProductionPlanDTO.cs
+++ DTO/ProductionPlanDTO.cs
@@ -228,7 +228,7 @@ namespace DM_OHD.DTO
                            BASE_OHD,
                            SHOT_CUMULATIVE,
                            CASE
-                               WHEN SHOT_CUMULATIVE < 30000000 THEN BASE_OHD
+                               WHEN SHOT_CUMULATIVE < 30000000 THEN 0
                                ELSE BASE_OHD + FLOOR(SHOT_CUMULATIVE / 30000000.0) * 30
                            END AS RAW_OHD
                     FROM agg
