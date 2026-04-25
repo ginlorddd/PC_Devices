2164b96c5b59cc20e2b6dd9130beeda59041f7c3
Pu <75680936+ginlorddd@users.noreply.github.com>
2026-04-25 08:56:13 +0700

    Polish waiting-approve UI while preserving layout structure

--- Forms/FRM_JIG_NEW_WAITING_APPROVE_LIST.cs
+++ Forms/FRM_JIG_NEW_WAITING_APPROVE_LIST.cs
@@ -1,5 +1,6 @@
 using JigFlow.Data;
 using System;
+using System.Windows.Forms;
 
 namespace JigFlow.Forms
 {
@@ -14,6 +15,7 @@ namespace JigFlow.Forms
 
         private void FRM_JIG_NEW_WAITING_APPROVE_LIST_Load(object sender, EventArgs e)
         {
+            txtMode.Text = "View";
             LoadData();
         }
 
@@ -26,6 +28,38 @@ namespace JigFlow.Forms
         {
             gcWaiting.DataSource = _service.GetWaitingApproveRequests();
             gvWaiting.BestFitColumns();
+            txtRecord.Text = gvWaiting.RowCount.ToString();
+            gvWaiting.Columns["REQUEST_AT"].DisplayFormat.FormatType = DevExpress.Utils.FormatType.DateTime;
+            gvWaiting.Columns["REQUEST_AT"].DisplayFormat.FormatString = "dd-MM-yy";
+        }
+
+        private void btnClose_Click(object sender, EventArgs e) => Close();
+
+        private void btnExport_Click(object sender, EventArgs e)
+        {
+            using (var dialog = new SaveFileDialog())
+            {
+                dialog.Filter = "Excel Workbook (*.xlsx)|*.xlsx";
+                dialog.FileName = $"Jig_New_Waiting_Approve_{DateTime.Now:yyyyMMdd_HHmm}.xlsx";
+                if (dialog.ShowDialog() != DialogResult.OK) return;
+                gcWaiting.ExportToXlsx(dialog.FileName);
+                MessageBox.Show("Đã xuất file thành công.", "Export", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Chức năng Update sẽ được triển khai theo workflow duyệt.", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Chức năng Delete sẽ được triển khai theo workflow duyệt.", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void btnApprove_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Chức năng Approve sẽ được triển khai theo workflow duyệt.", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Information);
         }
     }
 }
