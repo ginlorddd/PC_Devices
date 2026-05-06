9373c681e77dc4fece88e0586c334bd24c09c24d
Pu <75680936+ginlorddd@users.noreply.github.com>
2026-05-06 09:02:24 +0700

    Hoàn thiện form đăng ký hủy Jig và logic tìm kiếm/đăng ký

--- Forms/FRM_JIG_CANCEL_REGISTER.Designer.cs
+++ Forms/FRM_JIG_CANCEL_REGISTER.Designer.cs
@@ -3,7 +3,6 @@ namespace JigFlow.Forms
     partial class FRM_JIG_CANCEL_REGISTER
     {
         private System.ComponentModel.IContainer components = null;
-
         protected override void Dispose(bool disposing)
         {
             if (disposing && (components != null)) components.Dispose();
@@ -12,25 +11,72 @@ namespace JigFlow.Forms
 
         private void InitializeComponent()
         {
-            this.lblPlaceholder = new DevExpress.XtraEditors.LabelControl();
-            this.SuspendLayout();
-            this.lblPlaceholder.Appearance.Font = new System.Drawing.Font("Times New Roman", 14F, System.Drawing.FontStyle.Bold);
-            this.lblPlaceholder.Appearance.Options.UseFont = true;
-            this.lblPlaceholder.Location = new System.Drawing.Point(40, 40);
-            this.lblPlaceholder.Name = "lblPlaceholder";
-            this.lblPlaceholder.Size = new System.Drawing.Size(250, 26);
-            this.lblPlaceholder.Text = "Đăng ký hủy Jig - Đang cập nhật";
-            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 16F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1000, 600);
-            this.Controls.Add(this.lblPlaceholder);
-            this.Name = "FRM_JIG_CANCEL_REGISTER";
-            this.Text = "Đăng ký hủy Jig";
-            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
-            this.ResumeLayout(false);
-            this.PerformLayout();
+            this.lblTitle = new DevExpress.XtraEditors.LabelControl();
+            this.grpSearch = new DevExpress.XtraEditors.GroupControl();
+            this.btnSearch = new DevExpress.XtraEditors.SimpleButton();
+            this.lueManagementNo = new DevExpress.XtraEditors.LookUpEdit();
+            this.txtSearchManagementNo = new DevExpress.XtraEditors.TextEdit();
+            this.lblManagementNo = new DevExpress.XtraEditors.LabelControl();
+            this.grpJigInfo = new DevExpress.XtraEditors.GroupControl();
+            this.txtLocation = new DevExpress.XtraEditors.TextEdit();
+            this.txtUseProduct = new DevExpress.XtraEditors.TextEdit();
+            this.txtSize = new DevExpress.XtraEditors.TextEdit();
+            this.txtJigType = new DevExpress.XtraEditors.TextEdit();
+            this.txtFrequency = new DevExpress.XtraEditors.TextEdit();
+            this.txtJigName = new DevExpress.XtraEditors.TextEdit();
+            this.txtFactory = new DevExpress.XtraEditors.TextEdit();
+            this.txtDepartment = new DevExpress.XtraEditors.TextEdit();
+            this.grpReason = new DevExpress.XtraEditors.GroupControl();
+            this.memoNote = new DevExpress.XtraEditors.MemoEdit();
+            this.txtAbnormalNo = new DevExpress.XtraEditors.TextEdit();
+            this.deExpectedCancelDate = new DevExpress.XtraEditors.DateEdit();
+            this.lueReason = new DevExpress.XtraEditors.LookUpEdit();
+            this.btnRegister = new DevExpress.XtraEditors.SimpleButton();
+            this.btnClose = new DevExpress.XtraEditors.SimpleButton();
+            ((System.ComponentModel.ISupportInitialize)(this.grpSearch)).BeginInit(); this.grpSearch.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.lueManagementNo.Properties)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.txtSearchManagementNo.Properties)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.grpJigInfo)).BeginInit(); this.grpJigInfo.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.txtLocation.Properties)).BeginInit(); ((System.ComponentModel.ISupportInitialize)(this.txtUseProduct.Properties)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.txtSize.Properties)).BeginInit(); ((System.ComponentModel.ISupportInitialize)(this.txtJigType.Properties)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.txtFrequency.Properties)).BeginInit(); ((System.ComponentModel.ISupportInitialize)(this.txtJigName.Properties)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.txtFactory.Properties)).BeginInit(); ((System.ComponentModel.ISupportInitialize)(this.txtDepartment.Properties)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.grpReason)).BeginInit(); this.grpReason.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.memoNote.Properties)).BeginInit(); ((System.ComponentModel.ISupportInitialize)(this.txtAbnormalNo.Properties)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.deExpectedCancelDate.Properties.CalendarTimeProperties)).BeginInit(); ((System.ComponentModel.ISupportInitialize)(this.deExpectedCancelDate.Properties)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.lueReason.Properties)).BeginInit(); this.SuspendLayout();
+            this.lblTitle.Appearance.Font = new System.Drawing.Font("Times New Roman", 24F, System.Drawing.FontStyle.Bold); this.lblTitle.Appearance.Options.UseFont = true; this.lblTitle.Location = new System.Drawing.Point(12, 12); this.lblTitle.Text = "Đăng ký hủy Jig";
+            this.grpSearch.Controls.Add(this.btnSearch); this.grpSearch.Controls.Add(this.lueManagementNo); this.grpSearch.Controls.Add(this.txtSearchManagementNo); this.grpSearch.Controls.Add(this.lblManagementNo); this.grpSearch.Location = new System.Drawing.Point(12, 66); this.grpSearch.Size = new System.Drawing.Size(1360, 98); this.grpSearch.Text = "Search";
+            this.lblManagementNo.Location = new System.Drawing.Point(24, 50); this.lblManagementNo.Text = "Số quản lý";
+            this.txtSearchManagementNo.Location = new System.Drawing.Point(110, 46); this.txtSearchManagementNo.Size = new System.Drawing.Size(200, 22);
+            this.lueManagementNo.Location = new System.Drawing.Point(320, 46); this.lueManagementNo.Size = new System.Drawing.Size(470, 22);
+            this.btnSearch.Location = new System.Drawing.Point(1250, 40); this.btnSearch.Size = new System.Drawing.Size(90, 32); this.btnSearch.Text = "Search"; this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
+            this.grpJigInfo.Controls.AddRange(new System.Windows.Forms.Control[] { this.txtDepartment, this.txtFactory, this.txtJigName, this.txtFrequency, this.txtJigType, this.txtSize, this.txtUseProduct, this.txtLocation });
+            this.grpJigInfo.Location = new System.Drawing.Point(12, 170); this.grpJigInfo.Size = new System.Drawing.Size(1360, 230); this.grpJigInfo.Text = "Thông tin Jig";
+            int leftX = 210; int rightX = 930; int row1 = 45; int rowH = 40;
+            this.grpJigInfo.Controls.Add(new DevExpress.XtraEditors.LabelControl() { Location = new System.Drawing.Point(24, row1), Text = "Bộ phận" }); this.txtDepartment.Location = new System.Drawing.Point(leftX, row1 - 4); this.txtDepartment.Size = new System.Drawing.Size(450, 22);
+            this.grpJigInfo.Controls.Add(new DevExpress.XtraEditors.LabelControl() { Location = new System.Drawing.Point(24, row1 + rowH), Text = "Tên Jig" }); this.txtJigName.Location = new System.Drawing.Point(leftX, row1 + rowH - 4); this.txtJigName.Size = new System.Drawing.Size(450, 22);
+            this.grpJigInfo.Controls.Add(new DevExpress.XtraEditors.LabelControl() { Location = new System.Drawing.Point(24, row1 + rowH * 2), Text = "Loại Jig" }); this.txtJigType.Location = new System.Drawing.Point(leftX, row1 + rowH * 2 - 4); this.txtJigType.Size = new System.Drawing.Size(450, 22);
+            this.grpJigInfo.Controls.Add(new DevExpress.XtraEditors.LabelControl() { Location = new System.Drawing.Point(24, row1 + rowH * 3), Text = "Sản phẩm sử dụng" }); this.txtUseProduct.Location = new System.Drawing.Point(leftX, row1 + rowH * 3 - 4); this.txtUseProduct.Size = new System.Drawing.Size(450, 22);
+            this.grpJigInfo.Controls.Add(new DevExpress.XtraEditors.LabelControl() { Location = new System.Drawing.Point(24, row1 + rowH * 4), Text = "Vị trí" }); this.txtLocation.Location = new System.Drawing.Point(leftX, row1 + rowH * 4 - 4); this.txtLocation.Size = new System.Drawing.Size(450, 22);
+            this.grpJigInfo.Controls.Add(new DevExpress.XtraEditors.LabelControl() { Location = new System.Drawing.Point(760, row1), Text = "Nhà máy" }); this.txtFactory.Location = new System.Drawing.Point(rightX, row1 - 4); this.txtFactory.Size = new System.Drawing.Size(410, 22);
+            this.grpJigInfo.Controls.Add(new DevExpress.XtraEditors.LabelControl() { Location = new System.Drawing.Point(700, row1 + rowH), Text = "Tần suất kiểm tra" }); this.txtFrequency.Location = new System.Drawing.Point(rightX, row1 + rowH - 4); this.txtFrequency.Size = new System.Drawing.Size(410, 22);
+            this.grpJigInfo.Controls.Add(new DevExpress.XtraEditors.LabelControl() { Location = new System.Drawing.Point(760, row1 + rowH * 2), Text = "Size" }); this.txtSize.Location = new System.Drawing.Point(rightX, row1 + rowH * 2 - 4); this.txtSize.Size = new System.Drawing.Size(410, 22);
+            this.grpReason.Controls.AddRange(new System.Windows.Forms.Control[] { this.lueReason, this.deExpectedCancelDate, this.txtAbnormalNo, this.memoNote });
+            this.grpReason.Location = new System.Drawing.Point(12, 406); this.grpReason.Size = new System.Drawing.Size(1360, 230); this.grpReason.Text = "Nguyên nhân hủy";
+            this.grpReason.Controls.Add(new DevExpress.XtraEditors.LabelControl() { Location = new System.Drawing.Point(24, 50), Text = "Lí do hủy" }); this.lueReason.Location = new System.Drawing.Point(210, 46); this.lueReason.Size = new System.Drawing.Size(410, 22);
+            this.grpReason.Controls.Add(new DevExpress.XtraEditors.LabelControl() { Location = new System.Drawing.Point(660, 50), Text = "Ngày hủy dự kiến" }); this.deExpectedCancelDate.Location = new System.Drawing.Point(900, 46); this.deExpectedCancelDate.Size = new System.Drawing.Size(430, 22);
+            this.grpReason.Controls.Add(new DevExpress.XtraEditors.LabelControl() { Location = new System.Drawing.Point(24, 110), Text = "Số bất thường" }); this.txtAbnormalNo.Location = new System.Drawing.Point(210, 106); this.txtAbnormalNo.Size = new System.Drawing.Size(410, 22);
+            this.grpReason.Controls.Add(new DevExpress.XtraEditors.LabelControl() { Location = new System.Drawing.Point(790, 110), Text = "Ghi chú" }); this.memoNote.Location = new System.Drawing.Point(900, 106); this.memoNote.Size = new System.Drawing.Size(430, 100);
+            this.btnRegister.Location = new System.Drawing.Point(36, 658); this.btnRegister.Size = new System.Drawing.Size(160, 48); this.btnRegister.Text = "Register"; this.btnRegister.Click += new System.EventHandler(this.btnRegister_Click);
+            this.btnClose.Location = new System.Drawing.Point(210, 658); this.btnClose.Size = new System.Drawing.Size(160, 48); this.btnClose.Text = "Close"; this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+            this.ClientSize = new System.Drawing.Size(1384, 731); this.Controls.AddRange(new System.Windows.Forms.Control[] { this.lblTitle, this.grpSearch, this.grpJigInfo, this.grpReason, this.btnRegister, this.btnClose }); this.Name = "FRM_JIG_CANCEL_REGISTER"; this.Text = "Đăng ký hủy Jig"; this.Load += new System.EventHandler(this.FRM_JIG_CANCEL_REGISTER_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.grpSearch)).EndInit(); this.grpSearch.ResumeLayout(false); this.grpSearch.PerformLayout(); ((System.ComponentModel.ISupportInitialize)(this.lueManagementNo.Properties)).EndInit(); ((System.ComponentModel.ISupportInitialize)(this.txtSearchManagementNo.Properties)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.grpJigInfo)).EndInit(); this.grpJigInfo.ResumeLayout(false); this.grpJigInfo.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.txtLocation.Properties)).EndInit(); ((System.ComponentModel.ISupportInitialize)(this.txtUseProduct.Properties)).EndInit(); ((System.ComponentModel.ISupportInitialize)(this.txtSize.Properties)).EndInit(); ((System.ComponentModel.ISupportInitialize)(this.txtJigType.Properties)).EndInit(); ((System.ComponentModel.ISupportInitialize)(this.txtFrequency.Properties)).EndInit(); ((System.ComponentModel.ISupportInitialize)(this.txtJigName.Properties)).EndInit(); ((System.ComponentModel.ISupportInitialize)(this.txtFactory.Properties)).EndInit(); ((System.ComponentModel.ISupportInitialize)(this.txtDepartment.Properties)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.grpReason)).EndInit(); this.grpReason.ResumeLayout(false); this.grpReason.PerformLayout(); ((System.ComponentModel.ISupportInitialize)(this.memoNote.Properties)).EndInit(); ((System.ComponentModel.ISupportInitialize)(this.txtAbnormalNo.Properties)).EndInit(); ((System.ComponentModel.ISupportInitialize)(this.deExpectedCancelDate.Properties.CalendarTimeProperties)).EndInit(); ((System.ComponentModel.ISupportInitialize)(this.deExpectedCancelDate.Properties)).EndInit(); ((System.ComponentModel.ISupportInitialize)(this.lueReason.Properties)).EndInit(); this.ResumeLayout(false); this.PerformLayout();
         }
 
-        private DevExpress.XtraEditors.LabelControl lblPlaceholder;
+        private DevExpress.XtraEditors.LabelControl lblTitle; private DevExpress.XtraEditors.GroupControl grpSearch; private DevExpress.XtraEditors.SimpleButton btnSearch; private DevExpress.XtraEditors.LookUpEdit lueManagementNo; private DevExpress.XtraEditors.TextEdit txtSearchManagementNo; private DevExpress.XtraEditors.LabelControl lblManagementNo; private DevExpress.XtraEditors.GroupControl grpJigInfo; private DevExpress.XtraEditors.TextEdit txtLocation; private DevExpress.XtraEditors.TextEdit txtUseProduct; private DevExpress.XtraEditors.TextEdit txtSize; private DevExpress.XtraEditors.TextEdit txtJigType; private DevExpress.XtraEditors.TextEdit txtFrequency; private DevExpress.XtraEditors.TextEdit txtJigName; private DevExpress.XtraEditors.TextEdit txtFactory; private DevExpress.XtraEditors.TextEdit txtDepartment; private DevExpress.XtraEditors.GroupControl grpReason; private DevExpress.XtraEditors.MemoEdit memoNote; private DevExpress.XtraEditors.TextEdit txtAbnormalNo; private DevExpress.XtraEditors.DateEdit deExpectedCancelDate; private DevExpress.XtraEditors.LookUpEdit lueReason; private DevExpress.XtraEditors.SimpleButton btnRegister; private DevExpress.XtraEditors.SimpleButton btnClose;
     }
 }
--- Forms/FRM_JIG_CANCEL_REGISTER.cs
+++ Forms/FRM_JIG_CANCEL_REGISTER.cs
@@ -1,12 +1,166 @@
+using DevExpress.XtraEditors.Controls;
+using JigFlow.Data;
+using System;
+using System.Data;
+using System.Text;
 using System.Windows.Forms;
 
 namespace JigFlow.Forms
 {
     public partial class FRM_JIG_CANCEL_REGISTER : DevExpress.XtraEditors.XtraForm
     {
+        private readonly JigRegisterService _service = new JigRegisterService();
+
         public FRM_JIG_CANCEL_REGISTER()
         {
             InitializeComponent();
         }
+
+        private void FRM_JIG_CANCEL_REGISTER_Load(object sender, EventArgs e)
+        {
+            ConfigureLookUp();
+            ConfigureReadOnlyInfo();
+            LoadManagementNoLookup();
+            LoadReasonLookup();
+            deExpectedCancelDate.DateTime = DateTime.Today;
+        }
+
+        private void ConfigureLookUp()
+        {
+            lueManagementNo.Properties.TextEditStyle = TextEditStyles.Standard;
+            lueManagementNo.Properties.SearchMode = DevExpress.XtraEditors.Controls.SearchMode.AutoSearch;
+            lueManagementNo.Properties.PopupFilterMode = PopupFilterMode.Contains;
+            lueManagementNo.Properties.ImmediatePopup = true;
+            lueManagementNo.Properties.NullText = "Nhập/chọn số quản lý";
+
+            lueReason.Properties.TextEditStyle = TextEditStyles.DisableTextEditor;
+            lueReason.Properties.ShowHeader = false;
+            lueReason.Properties.ShowFooter = false;
+            lueReason.Properties.NullText = "Chọn lý do hủy";
+        }
+
+        private void ConfigureReadOnlyInfo()
+        {
+            foreach (var EDIT in new[] { txtDepartment, txtFactory, txtJigName, txtFrequency, txtJigType, txtSize, txtUseProduct, txtLocation })
+            {
+                EDIT.Properties.ReadOnly = true;
+            }
+        }
+
+        private void LoadManagementNoLookup()
+        {
+            var DT = _service.GetJigsAvailableForCancel();
+            lueManagementNo.Properties.DataSource = DT;
+            lueManagementNo.Properties.DisplayMember = "CONTROL_NO";
+            lueManagementNo.Properties.ValueMember = "CONTROL_NO";
+            lueManagementNo.Properties.Columns.Clear();
+            lueManagementNo.Properties.Columns.Add(new DevExpress.XtraEditors.Controls.LookUpColumnInfo("CONTROL_NO", "Số quản lý"));
+            lueManagementNo.Properties.Columns.Add(new DevExpress.XtraEditors.Controls.LookUpColumnInfo("JIG_NAME", "Tên Jig"));
+            lueManagementNo.Properties.Columns.Add(new DevExpress.XtraEditors.Controls.LookUpColumnInfo("USE_SECTION", "Bộ phận"));
+        }
+
+        private void LoadReasonLookup()
+        {
+            var DT = new DataTable();
+            DT.Columns.Add("VALUE");
+            DT.Rows.Add("Hỏng không thể sửa");
+            DT.Rows.Add("Không còn nhu cầu sử dụng");
+            DT.Rows.Add("Thay thế bằng Jig mới");
+            DT.Rows.Add("Khác");
+
+            lueReason.Properties.DataSource = DT;
+            lueReason.Properties.DisplayMember = "VALUE";
+            lueReason.Properties.ValueMember = "VALUE";
+            lueReason.Properties.Columns.Clear();
+            lueReason.Properties.Columns.Add(new LookUpColumnInfo("VALUE", "Lý do hủy"));
+        }
+
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+            var MANAGEMENT_NO = Convert.ToString(lueManagementNo.EditValue);
+            if (string.IsNullOrWhiteSpace(MANAGEMENT_NO)) MANAGEMENT_NO = txtSearchManagementNo.Text.Trim();
+            if (string.IsNullOrWhiteSpace(MANAGEMENT_NO))
+            {
+                MessageBox.Show("Vui lòng nhập Số quản lý.", "Thiếu dữ liệu", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            var ROW = _service.GetJigMasterByControlNo(MANAGEMENT_NO);
+            if (ROW == null)
+            {
+                ClearJigInfo();
+                MessageBox.Show("Không tìm thấy Jig đang sử dụng với số quản lý đã nhập.", "Không tìm thấy", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            txtSearchManagementNo.Text = Convert.ToString(ROW["CONTROL_NO"]);
+            lueManagementNo.EditValue = Convert.ToString(ROW["CONTROL_NO"]);
+            txtDepartment.Text = Convert.ToString(ROW["USE_SECTION"]);
+            txtFactory.Text = Convert.ToString(ROW["FACTORY"]);
+            txtJigName.Text = Convert.ToString(ROW["JIG_NAME"]);
+            txtFrequency.Text = Convert.ToString(ROW["CHECK_FREQUENCY"]);
+            txtJigType.Text = Convert.ToString(ROW["JIG_TYPE"]);
+            txtSize.Text = Convert.ToString(ROW["JIG_SIZE"]);
+            txtUseProduct.Text = Convert.ToString(ROW["USE_PRODUCT"]);
+            txtLocation.Text = Convert.ToString(ROW["LOCATION_CODE"]);
+        }
+
+        private void ClearJigInfo()
+        {
+            txtDepartment.Text = string.Empty;
+            txtFactory.Text = string.Empty;
+            txtJigName.Text = string.Empty;
+            txtFrequency.Text = string.Empty;
+            txtJigType.Text = string.Empty;
+            txtSize.Text = string.Empty;
+            txtUseProduct.Text = string.Empty;
+            txtLocation.Text = string.Empty;
+        }
+
+        private void btnRegister_Click(object sender, EventArgs e)
+        {
+            var MANAGEMENT_NO = txtSearchManagementNo.Text.Trim();
+            if (string.IsNullOrWhiteSpace(MANAGEMENT_NO) || string.IsNullOrWhiteSpace(txtJigName.Text))
+            {
+                MessageBox.Show("Vui lòng Search Jig trước khi đăng ký hủy.", "Thiếu dữ liệu", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            var REASON = Convert.ToString(lueReason.EditValue);
+            if (string.IsNullOrWhiteSpace(REASON))
+            {
+                MessageBox.Show("Vui lòng chọn Lý do hủy.", "Thiếu dữ liệu", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            var ROW = _service.GetJigMasterByControlNo(MANAGEMENT_NO);
+            if (ROW == null)
+            {
+                MessageBox.Show("Jig không còn hợp lệ để đăng ký hủy.", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            var jigId = Convert.ToInt32(ROW["JIG_ID"]);
+            if (_service.HasWaitingCancelRequest(jigId))
+            {
+                MessageBox.Show("Jig này đã có yêu cầu hủy chờ duyệt.", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            var detail = new StringBuilder();
+            detail.Append(REASON);
+            if (!string.IsNullOrWhiteSpace(txtAbnormalNo.Text)) detail.Append($" | Số bất thường: {txtAbnormalNo.Text.Trim()}");
+            detail.Append($" | Ngày hủy dự kiến: {deExpectedCancelDate.DateTime:yyyy-MM-dd}");
+            if (!string.IsNullOrWhiteSpace(memoNote.Text)) detail.Append($" | Ghi chú: {memoNote.Text.Trim()}");
+
+            var requestBy = string.IsNullOrWhiteSpace(AppSession.UserId) ? "SYSTEM" : AppSession.UserId;
+            _service.CreateCancelRequest(jigId, detail.ToString(), requestBy);
+
+            MessageBox.Show("Đã đăng ký hủy Jig, vui lòng chờ phê duyệt.", "Thành công", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            DataChangeNotifier.Notify("JIG_CANCEL_REQUEST");
+            Close();
+        }
+
+        private void btnClose_Click(object sender, EventArgs e) => Close();
     }
 }
