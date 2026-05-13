40421ad69a3d718f725111f4f6bd79522c9c1330
Pu <75680936+ginlorddd@users.noreply.github.com>
2026-05-13 10:25:29 +0700

    Redesign login form with remember and show password options

--- Forms/FRM_LOGIN.Designer.cs
+++ Forms/FRM_LOGIN.Designer.cs
@@ -17,8 +17,12 @@ namespace JigFlow.Forms
             this.btnClose = new DevExpress.XtraEditors.SimpleButton();
             this.lblUsername = new DevExpress.XtraEditors.LabelControl();
             this.lblPassword = new DevExpress.XtraEditors.LabelControl();
+            this.chkRememberPassword = new DevExpress.XtraEditors.CheckEdit();
+            this.chkShowPassword = new DevExpress.XtraEditors.CheckEdit();
             ((System.ComponentModel.ISupportInitialize)(this.txtUsername.Properties)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.txtPassword.Properties)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chkRememberPassword.Properties)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chkShowPassword.Properties)).BeginInit();
             this.SuspendLayout();
             this.lblUsername.Location = new System.Drawing.Point(28, 26);
             this.lblUsername.Text = "Tài khoản";
@@ -29,19 +33,28 @@ namespace JigFlow.Forms
             this.txtPassword.Location = new System.Drawing.Point(108, 59);
             this.txtPassword.Size = new System.Drawing.Size(230, 22);
             this.txtPassword.Properties.UseSystemPasswordChar = true;
-            this.btnLogin.Location = new System.Drawing.Point(108, 100);
+            this.chkRememberPassword.Location = new System.Drawing.Point(108, 90);
+            this.chkRememberPassword.Properties.Caption = "Lưu mật khẩu";
+            this.chkRememberPassword.Size = new System.Drawing.Size(110, 20);
+            this.chkShowPassword.Location = new System.Drawing.Point(228, 90);
+            this.chkShowPassword.Properties.Caption = "Hiển thị mật khẩu";
+            this.chkShowPassword.Size = new System.Drawing.Size(120, 20);
+            this.chkShowPassword.CheckedChanged += new System.EventHandler(this.chkShowPassword_CheckedChanged);
+            this.btnLogin.Location = new System.Drawing.Point(108, 120);
             this.btnLogin.Size = new System.Drawing.Size(100, 30);
             this.btnLogin.Text = "Đăng nhập";
             this.btnLogin.Click += new System.EventHandler(this.btnLogin_Click);
-            this.btnClose.Location = new System.Drawing.Point(238, 100);
+            this.btnClose.Location = new System.Drawing.Point(238, 120);
             this.btnClose.Size = new System.Drawing.Size(100, 30);
             this.btnClose.Text = "Đóng";
             this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
-            this.ClientSize = new System.Drawing.Size(376, 157);
+            this.ClientSize = new System.Drawing.Size(376, 170);
             this.Controls.Add(this.lblUsername);
             this.Controls.Add(this.txtUsername);
             this.Controls.Add(this.lblPassword);
             this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.chkRememberPassword);
+            this.Controls.Add(this.chkShowPassword);
             this.Controls.Add(this.btnLogin);
             this.Controls.Add(this.btnClose);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
@@ -50,6 +63,8 @@ namespace JigFlow.Forms
             this.Text = "Đăng nhập";
             ((System.ComponentModel.ISupportInitialize)(this.txtUsername.Properties)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.txtPassword.Properties)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chkRememberPassword.Properties)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chkShowPassword.Properties)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
         }
@@ -60,5 +75,7 @@ namespace JigFlow.Forms
         private DevExpress.XtraEditors.SimpleButton btnClose;
         private DevExpress.XtraEditors.LabelControl lblUsername;
         private DevExpress.XtraEditors.LabelControl lblPassword;
+        private DevExpress.XtraEditors.CheckEdit chkRememberPassword;
+        private DevExpress.XtraEditors.CheckEdit chkShowPassword;
     }
 }
